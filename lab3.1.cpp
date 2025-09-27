bd05c13a80119085b14b5fc369fc31f3ee956ace
Dmytroqght <dmytro.ganja.kn@dspu.edu.ua>
2025-09-27 22:42:30 +0300

    lab3.1.cpp

---
+++ lab3.1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	double y;
+	double x;
+	double e = 2.71;
+	cout << "Enter x: ";
+	cin >> x;
+	if (x <= -1) {
+		y = 2 * fabs(5 - x) - pow(e,fabs(2+x)); // powf is used for float, pow for double
+	}
+	else if (x > -1 && x < 1) {
+		y = 2 * fabs(5 - x) - sin(1 / (fabs(2 + x)))* sin(1 / (fabs(2 + x)));
+	}
+	else if (x >= 2) {
+		y = 2 * fabs(5 - x) - (cos(x) * cos(x) / (1 + fabs(sin(x))));
+	}
+	//y = 2 * fabs(5 - x) - pow(e,fabs(2+x)); // powf is used for float, pow for double
+	//y = 2 * fabs(5 - x) - sin(1 / (fabs(2 + x)))* sin(1 / (fabs(2 + x)));
+	//y = 2 * fabs(5 - x) - (cos(x) * cos(x) / (1 + fabs(sin(x))));
+	cout << "y = " << y << endl;
+
+
+}
+
